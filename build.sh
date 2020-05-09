c2a36235f4f12a2e48917d0561659a6fcc34541b
vesim987 <vesim809@pm.me>
2020-05-09 17:45:18 +0200

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
 
 REPO_PATH="$(dirname $(readlink -f "$0"))"
-ls -la ${REPO_PATH}/*
-
-echo make -C "${REPO_PATH}/buildroot" BR2_EXTERNAL="${REPO_PATH}/" O="${REPO_PATH}/output/" $*
 make -C "${REPO_PATH}/buildroot" BR2_EXTERNAL="${REPO_PATH}/" O="${REPO_PATH}/output/" $*
